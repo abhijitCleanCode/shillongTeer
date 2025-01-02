8e45d52c0053b8709bf979ef21149c04a69bdef6
abhijit kumar singh <abhijitkumarsingh.cse58@gmail.com>
2025-01-02 09:32:17 +0530

    first commit

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 import express from "express";
-import cors from 'cors'
-import morgan from 'morgan'
+import cors from "cors";
+import morgan from "morgan";
 const app = express();
 import connect from "./connect.js";
 import { router } from "./router.js";
 
 //middleware
-app.use(express.json())
-app.use(cors(
-    {
-        origin: "*",
-        methods: ["POST", "GET"],
-        credential: true
-    }
-))
-app.use(morgan('tiny'))
-app.disable('x-powered-by')
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET"],
+    credential: true,
+  })
+);
+app.use(morgan("tiny"));
+app.disable("x-powered-by");
 
-app.use('/api', router)
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 const port = process.env.PORT || 8000;
 
-connect().then(() => {
-    app.get('/', (req, res) => {
-        res.status(200).json("Server started at port " + port);
-    })
+connect()
+  .then(() => {
+    app.get("/", (req, res) => {
+      res.status(200).json("Server started at port " + port);
+    });
     app.listen(port, (req, res) => {
-        console.log("Server started at port " + port)
-    })
-}).catch(e => console.log(e));
+      console.log("Server started at port " + port);
+    });
+  })
+  .catch((e) => console.log(e));
